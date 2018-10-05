9744c794a081ab17d81284129a80f44b2d16b122
ShengyuZhang <i@silverrainz.me>
2018-10-05 16:24:47 +0800

    Config module and exmaple config file

---
+++ labots/config/config.py
@@ -0,0 +1,75 @@
+import logging
+import yaml
+import pprint
+from typing import Dict
+
+from labots.config import checker
+
+# Initialize logging
+logger = logging.getLogger(__name__)
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+class Config(AttrDict):
+    pass
+
+
+def load_config(raw: str) -> Config:
+    """ Load config from a yaml format string.  """
+    d = yaml.load(raw)
+    try:
+        checker.check(d, [
+            checker.Item(key = ['irc', 'host'],
+                checkers = [checker.is_str, checker.is_not_empty_str],
+                required = True),
+            checker.Item(key = ['irc', 'port'],
+                checkers = [checker.is_int, checker.is_port],
+                default = 6667,
+                required = True),
+            checker.Item(key = ['irc', 'tls'],
+                checkers = [checker.is_bool],
+                default = False),
+            checker.Item(key = ['irc', 'tls_verify'],
+                checkers = [checker.is_bool],
+                default = True),
+            checker.Item(key = ['irc', 'nickname'],
+                checkers = [checker.is_str, checker.is_not_empty_str],
+                default = 'labots'),
+            checker.Item(key = ['irc', 'username'],
+                checkers = [checker.is_str, checker.is_not_empty_str],
+                default = 'labots'),
+            checker.Item(key = ['irc', 'realname'],
+                checkers = [checker.is_str, checker.is_not_empty_str],
+                default = 'https://github.com/SilverRainZ/labots'),
+            checker.Item(key = ['irc', 'password'],
+                checkers = [checker.is_str],
+                default = ''),
+
+            checker.Item(key = ['manager', 'path'],
+                checkers = [checker.is_str],
+                required = True),
+            checker.Item(key = ['manager', 'config'],
+                checkers = [checker.is_str]),
+
+            checker.Item(key = ['server', 'listen'],
+                checkers = [checker.is_str],
+                default = 'unix:///tmp/labots.sock'),
+            ])
+    except (KeyError, ValueError) as e:
+        raise e
+
+    return Config(_build_attr_dict(d))
+
+
+def _build_attr_dict(d: Dict) -> AttrDict:
+    """ Recursively convert all dict to AttrDict. """
+    r = {}
+    for k, v in d.items():
+        if isinstance(v, dict):
+            r[k] = _build_attr_dict(v)
+        else:
+            r[k] = v
+    return AttrDict(r)

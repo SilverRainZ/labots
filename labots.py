44ac2af0b1ca5d4bd537050b311407789f867f4d
Shengyu Zhang <i@silverrainz.me>
2018-10-11 20:52:15 +0800

    Run server and client separately according to subcommands

--- labots.py
+++ labots.py
@@ -1,32 +1,30 @@
 #!/usr/bin/env python3
 # -*- encoding: UTF-8 -*-
 
+import sys
 import argparse
 import logging
+from tornado.ioloop import IOLoop
 
 from labots.config import config
 from labots.irc import client
 from labots.bot import manager
-from labots.api import server
+from labots.api import server as apiserver
+from labots.api import client as apiclient
 from labots.utils import clogger
+from labots.common import meta
 
 # Initialize logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--config",
-            default = 'labots.yaml',
-            type = str,
-            help = "specified a config file")
-    args = parser.parse_args()
+def labots_server(args: argparse.Namespace):
     with open(args.config, 'r') as f:
         try:
             cfg = config.load_config(f.read())
         except (KeyError, ValueError) as e:
             logger.error(e)
-            return 1
+            return
 
     irc = client.Client(
             host = cfg.irc.host,
@@ -39,7 +37,7 @@ def main():
     mgr = manager.Manager(
             path = cfg.manager.path,
             config_path = cfg.manager.config)
-    api = server.Server(
+    api = apiserver.Server(
             listen = cfg.server.listen,
             manager = mgr)
     mgr.action = irc
@@ -52,11 +50,67 @@ def main():
         irc.handle()
     except KeyboardInterrupt:
         mgr.unload_bots()
-        api.stop()
+        api.close()
         irc.disconnect()
 
+def labots_client(args: argparse.Namespace):
+    def callback():
+        api = apiclient.Client(addr = args.addr)
+        ret = api.request(args.bot, args.action)
+        api.close()
+        IOLoop.instance().stop()
+        sys.exit(0 if ret else 1)
+
+    IOLoop.instance().add_callback(callback())
+    IOLoop.instance().start()
+
+
+def main():
+    parser = argparse.ArgumentParser(description = meta.description)
+    parser.add_argument("-v", "--version",
+            action = 'version',
+            version = meta.pretty_name + ' ' + meta.version)
+
+    subparsers = parser.add_subparsers(title = 'subcommanads')
+
+    # Server argumentes
+    srvparser = subparsers.add_parser('server',
+            aliases = ['srv', 's'],
+            help = 'start the server')
+    srvparser.add_argument("-c", "--config",
+            default = meta.name + '.yaml',
+            type = str,
+            help = "specify the path of configuration file")
+    srvparser.set_defaults(func = labots_server)
+
+    # Client argumentes
+    cliparser = subparsers.add_parser('client',
+            aliases = ['cli', 'c'],
+            help = 'start the client')
+    cliparser.add_argument("-a", "--addr",
+            default = meta.default_listen,
+            type = str,
+            help = 'specify the address that the server listens on')
+    cliparser.add_argument('action',
+            metavar = 'ACTION',
+            type = str,
+            help = 'specify the action to perform')
+    cliparser.add_argument('bot',
+            metavar = 'BOT',
+            type = str,
+            help = 'specify the name of bot')
+    cliparser.set_defaults(func = labots_client)
+
+    subparsers.default = cliparser
+    args = parser.parse_args()
+    if hasattr(args, 'func'):
+        args.func(args)
+    else:
+        parser.print_help()
+
+
 if __name__ == '__main__':
-    filelog = logging.FileHandler('labots.log')
+    filelog = logging.FileHandler(meta.name + '.log')
     filelog.setFormatter(clogger.Formatter(False))
     syslog = logging.StreamHandler()
     syslog.setFormatter(clogger.Formatter(True))
